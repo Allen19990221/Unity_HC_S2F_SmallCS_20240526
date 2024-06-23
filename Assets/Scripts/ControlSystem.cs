4d483e4fd0e4d626cbd13208c0494ae88f216715
KID <50666207+KID421@users.noreply.github.com>
2024-06-23 16:00:43 +0800

    Update 添加攝影機跟隨效果

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -14,11 +14,34 @@ namespace KID
         private float moveSpeed = 3.5f;
 
         private Rigidbody2D rig;
+        private Animator ani;
+        private string parMove = "移動數值";
 
         private void Awake()
         {
             // 獲得此物件身上的 2D 剛體並存放到變數 rig 內
             rig = GetComponent<Rigidbody2D>();
+            ani = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            // 呼叫自訂方法移動
+            Move();
+        }
+
+        // 自訂方法：移動
+        private void Move()
+        {
+            // 獲得玩家的水平按鍵：A、D 與左右
+            // 玩家按下左 -1，右 +1，沒按 0
+            float h = Input.GetAxis("Horizontal");
+            // 剛體的加速度 = 玩家水平按鍵 * 移動速度，Y 軸是原本的重力
+            rig.velocity = new Vector2(h * moveSpeed, rig.velocity.y);
+            // 對 h 取絕對值
+            h = Mathf.Abs(h);
+            // 設定浮點數參數 為 h
+            ani.SetFloat(parMove, h);
         }
     }
 }
